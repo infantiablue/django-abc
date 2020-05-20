5d5098cf155984e0547d990a3ddeccb27d53fe09
Truong Phan <dangtruong@gmail.com>
2020-05-20 08:46:24 +0700

    Displaying courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,3 +1,6 @@
+from django.views.generic.detail import DetailView
+from django.db.models import Count
+from .models import Subject
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 import sys
 import os
@@ -178,3 +181,25 @@ class ContentOrderView(CsrfExemptMixin,
                                    module__course__owner=request.user) \
                 .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+            total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+            total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
